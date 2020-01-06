72d42e86fbd30a5475b08dcc346ac4698d4322e8
Radityo Putra <radityo.putra@gmail.com>
2020-01-06 17:44:23 +0700

    bug jobs

--- app/Http/Controllers/Admin/Auth/AuthController.php
+++ app/Http/Controllers/Admin/Auth/AuthController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rule;
 use App\Notifications\Admin\RegisterNotification;
-use App\JobS\Admin\ProcessSendEmailAdminRegister;
+use App\Jobs\Admin\ProcessSendEmailAdminRegister;
 
 class AuthController extends Controller
 {
